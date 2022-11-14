c468fef5466d430e5e71107370bb52f594c5216d
Alex Pry <alhaffthemage@gmail.com>
2022-11-14 09:36:34 +0200

    mainWindow File with code for lab2 lab

---
+++ ComputerGraphics/MainWindowLab2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ComputerGraphics
+{
+    public partial class MainWindow : Window
+    {
+        private void Lab2_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Lab2_Unselected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Lab2Tangent_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Lab2Tangent_Unselected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
