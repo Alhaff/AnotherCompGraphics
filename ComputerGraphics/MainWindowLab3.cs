29e2a83952baab113897d90d087c106ca11bb392
Alex Pry <alhaffthemage@gmail.com>
2022-11-14 09:36:42 +0200

    mainWindow File with code for lab3 lab

---
+++ ComputerGraphics/MainWindowLab3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ComputerGraphics
+{
+    public partial class MainWindow : Window
+    {
+        private void Lab3_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Lab3_Unselected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
