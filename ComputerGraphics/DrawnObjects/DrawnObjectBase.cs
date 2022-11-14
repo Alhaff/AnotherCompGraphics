b9f14c5ea5b20c000ddee1557213d4d17ceaae16
Alex Pry <alhaffthemage@gmail.com>
2022-11-14 09:19:04 +0200

    Base class which must inherite all objects what you want draw

---
+++ ComputerGraphics/DrawnObjects/DrawnObjectBase.cs
@@ -0,0 +1,30 @@
+﻿using ComputerGraphics.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ComputerGraphics.DrawnObjects
+{
+    public abstract class DrawnObjectBase : MyNotifyPropertyChanged
+    {
+        public int DrawPriority { get; set; } = 0;
+        public Color MyColor { get; set; } = Colors.Black;
+        public bool IsPlaneTransformMe { get; set; } = true;
+
+        public Transformation TransformMe = p => p;
+
+        protected abstract IEnumerable<IEnumerable<Vector>> ObjectContourPoints();
+
+        public IEnumerable<IEnumerable<Vector>> GetContourPoints()
+        {
+            foreach (var countour in ObjectContourPoints())
+                yield return countour.Select(point => TransformMe(point));
+        }
+    }
+}
