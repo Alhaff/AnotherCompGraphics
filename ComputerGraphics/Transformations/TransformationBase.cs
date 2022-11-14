04e48e8f98daefff7280bb86a1a31b3cf1edcfcf
Alex Pry <alhaffthemage@gmail.com>
2022-11-14 09:31:14 +0200

    Base class for transformations

---
+++ ComputerGraphics/Transformations/TransformationBase.cs
@@ -0,0 +1,16 @@
+﻿using ComputerGraphics.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerGraphics.Transformations
+{
+    public abstract class TransformationBase : MyNotifyPropertyChanged
+    {
+        public abstract Transformation Transform { get; }
+
+        public static implicit operator Transformation(TransformationBase tr) => tr.Transform;
+    }
+}
