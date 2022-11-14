2064ed40fd137b30d2ddc5f826f794c5183e24bd
Alex Pry <alhaffthemage@gmail.com>
2022-11-14 09:30:33 +0200

    Different classes which helped in program work

---
+++ ComputerGraphics/Helpers/IEnumerableExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ComputerGraphics.Helpers
+{
+    public static class IEnumerableExtensions
+    {
+        static private bool PointBelongsStraightLine(Vector lineStart, Vector lineEnd, Vector point)
+        {
+            double diff = (point.X - lineStart.X) / (lineEnd.X - lineStart.X) -
+                          (point.Y - lineStart.Y) / (lineEnd.Y - lineStart.Y);
+            return diff > 0 - 1E-10 && diff < 0 + 1E-10;
+        }
+        public static IEnumerable<Tuple<Vector, Vector>> LineCreator(this IEnumerable<Vector> items)
+        {
+            var line = new LinkedList<Vector>();
+            foreach (var item in items)
+            {
+                if (line.Count == 2)
+                {
+                    if (PointBelongsStraightLine(line.First.Value, line.Last.Value, item))
+                    {
+                        line.RemoveLast();
+                    }
+                    else
+                    {
+                        yield return Tuple.Create(line.First.Value, line.Last.Value);
+                        line.RemoveFirst();
+                    }
+                }
+                line.AddLast(item);
+            }
+            if (line.Count > 0)
+                yield return Tuple.Create(line.First.Value, line.Last.Value);
+        }
+    }
+}
---
+++ ComputerGraphics/Helpers/VectorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ComputerGraphics.Helpers
+{
+    static public class VectorExtensions
+    {
+        public static Vector Rotate(this Vector start, double angle)
+        {
+            float newX = (float)(start.X * Math.Cos(angle) - start.Y * Math.Sin(angle));
+            float newY = (float)(start.X * Math.Sin(angle) + start.Y * Math.Cos(angle));
+            return new Vector(newX, newY);
+        }
+        public static double Angle(this Vector first) => Math.Atan2(first.Y, first.X);
+
+        public static bool IsSamePoint(this Vector point, Vector otherPoint, double precision)
+        {
+            double diffX = point.X - otherPoint.X;
+            double diffY = point.Y - otherPoint.Y;
+            Func<double, bool> IsDiffAroundZero = (diff) => diff > -precision && diff < precision;
+            return IsDiffAroundZero(diffX) && IsDiffAroundZero(diffY);
+        }
+    }
+}
