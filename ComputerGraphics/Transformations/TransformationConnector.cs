b9a6a0f46aa7915de2cc423e36bd6197e9fb1390
Alex Pry <alhaffthemage@gmail.com>
2022-11-14 09:32:01 +0200

    Class which connect different transformation together

---
+++ ComputerGraphics/Transformations/TransformationConnector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerGraphics.Transformations
+{
+    public class TransformationConnector : TransformationBase
+    {
+        public readonly Transformation[] Transformations;
+        public TransformationConnector(params Transformation[] transformations)
+        {
+            Transformations = transformations;
+        }
+        public override Transformation Transform => v=>
+        {
+            foreach (var transformation in Transformations)
+            {
+                v = transformation(v);
+            }
+            return v;
+        };
+    }
+}
